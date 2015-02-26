3674db11585c5539936a33eaae3eaa46832cb34e
Rckdrigo <rckdigomed@gmail.com>
2015-02-26 14:16:49 -0600

    Nuevo sistema de generacion de obstaculos y clumnas

---
+++ Assets/ObstaclePerLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstaclePerLevel : MonoBehaviour {
+	public string[] obstacles;
+
+	[Range(0,100)]
+	public int probability = 50;
+
+	[Range(0.5f,2f)]
+	public float time = 1f;
+
+	public float delay = 0f;
+}
